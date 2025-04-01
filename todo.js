9a722d5a30e7537aeae05d262f11bf8350f0bce7
Hadi Shah <hadishah.work@gmail.com>
2025-04-01 21:30:34 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -8,19 +8,7 @@ let todoList = [
     dueDate: '4/10/2023'
   }
 ];
-
 displayItems();
-
-function addTodo() {
-  let inputElement = document.querySelector('#todo-input');
-  let dateElement = document.querySelector('#todo-date');
-  let todoItem = inputElement.value;
-  let todoDate = dateElement.value;
-  todoList.push({item: todoItem, dueDate: todoDate});
-  inputElement.value = '';
-  dateElement.value = '';
-  displayItems();
-}
 
 function displayItems() {
   let containerElement = document.querySelector('.todo-container');
@@ -35,4 +23,15 @@ function displayItems() {
     `;
   }
   containerElement.innerHTML = newHtml;
+}
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+  displayItems();
 }
